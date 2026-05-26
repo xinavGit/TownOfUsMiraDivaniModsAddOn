9c833eb05163128f9dcea7eeaa860e39964141c1
DivaniNL <dylanvannierop@gmail.com>
2026-05-26 22:47:15 +0200

    renamed variable name for shuffle dead bodies. mira preset resets

--- Options/Modifiers/Game/Universal/ShuffleOptions.cs
+++ Options/Modifiers/Game/Universal/ShuffleOptions.cs
@@ -20,8 +20,8 @@ public class ShuffleOptions : AbstractOptionGroup<ShuffleModifier>
     public ModdedNumberOption ShuffleCooldown { get; } =
         new("Shuffle Cooldown", 30f, 10f, 60f, 5f, MiraNumberSuffixes.Seconds);
     
-    public ModdedToggleOption ShuffleDeadBodiesOption { get; } =
+    public ModdedToggleOption ShuffleCorpsesOption { get; } =
         new("Shuffle Dead Bodies", true);
-    
-    public bool ShuffleDeadBodies => ShuffleDeadBodiesOption.Value;
+
+    public bool ShuffleCorpses => ShuffleCorpsesOption.Value;
 }
